58f65310f26215d470d0141620ef14f889708b5e
Diego Granados <diego@codeando.edu.pe>
2022-01-19 14:29:45 -0400

    se agrego sistema en que el CPU elige su opcion

--- app.js
+++ app.js
@@ -7,4 +7,18 @@ const getUserChoice = userInput => {
     }
   };
   
-  console.log(getUserChoice ('rocks'));
+  
+  function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random()*3);
+    switch (randomNumber) {
+    case 0:
+      return 'rock';
+      break;
+    case 1:
+      return 'paper';
+      break;
+    case 2:
+     return 'scissors';
+     break;
+    }
+  }
