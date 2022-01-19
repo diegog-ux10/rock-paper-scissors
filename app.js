9b59ea212b56ba983218eb0a51d144ad4cf0ed46
Diego Granados <diego@codeando.edu.pe>
2022-01-19 15:10:37 -0400

    Se establecio proceso para seleccionar ganador

--- app.js
+++ app.js
@@ -21,4 +21,38 @@ const getUserChoice = userInput => {
      return 'scissors';
      break;
     }
+  };
+  
+  function determineWinner (userChoice, computerChoice) {
+  
+    if (userChoice === computerChoice){
+      return 'The game is a tie!';
+    }
+    else {
+        if (userChoice === 'rock'){
+          if(computerChoice === 'paper'){
+            return 'The computer won!'
+          }
+          else {
+            return 'You won!';
+          }
+        }
+        else if (userChoice === 'paper') {
+          if(computerChoice === 'scissors'){
+            return 'The computer won!'
+          }
+          else {
+            return 'You won!';
+          }
+        }
+        else if (userChoice === 'scissors') {
+          if(computerChoice === 'rock'){
+            return 'The computer won!'
+          }
+          else {
+            return 'You won!';
+          }
+        }
+    }
+  
   }
