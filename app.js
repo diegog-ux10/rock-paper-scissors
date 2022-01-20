c0c9041cc54c6cc7ed7284726c8d0b4c9cf3b320
Diego Granados <diego@codeando.edu.pe>
2022-01-20 09:01:30 -0400

    Se agregó la funcion para jugar

--- app.js
+++ app.js
@@ -1,58 +1,68 @@
 const getUserChoice = userInput => {
-    userInput = userInput.toLowerCase();
-    if (userInput === 'rock' || userInput === 'paper' || userInput ==='scissors') {
-      return userInput;
-    } else {
-      console.log('Error!');
-    }
-  };
-  
-  
-  function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random()*3);
-    switch (randomNumber) {
-    case 0:
-      return 'rock';
-      break;
-    case 1:
-      return 'paper';
-      break;
-    case 2:
-     return 'scissors';
-     break;
-    }
-  };
-  
-  function determineWinner (userChoice, computerChoice) {
-  
-    if (userChoice === computerChoice){
-      return 'The game is a tie!';
-    }
-    else {
-        if (userChoice === 'rock'){
-          if(computerChoice === 'paper'){
-            return 'The computer won!'
-          }
-          else {
-            return 'You won!';
-          }
-        }
-        else if (userChoice === 'paper') {
-          if(computerChoice === 'scissors'){
-            return 'The computer won!'
-          }
-          else {
-            return 'You won!';
-          }
-        }
-        else if (userChoice === 'scissors') {
-          if(computerChoice === 'rock'){
-            return 'The computer won!'
-          }
-          else {
-            return 'You won!';
-          }
-        }
-    }
-  
+  userInput = userInput.toLowerCase();
+  if (userInput === 'rock' || userInput === 'paper' || userInput ==='scissors') {
+    return userInput;
+  } else {
+    console.log('Error!');
   }
+};
+
+
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random()*3);
+  switch (randomNumber) {
+  case 0:
+    return 'rock';
+    break;
+  case 1:
+    return 'paper';
+    break;
+  case 2:
+   return 'scissors';
+   break;
+  }
+};
+
+function determineWinner (userChoice, computerChoice) {
+
+  if (userChoice === computerChoice){
+    return 'The game is a tie!';
+  }
+  else {
+      if (userChoice === 'rock'){
+        if(computerChoice === 'paper'){
+          return 'The computer won!'
+        }
+        else {
+          return 'You won!';
+        }
+      }
+      else if (userChoice === 'paper') {
+        if(computerChoice === 'scissors'){
+          return 'The computer won!'
+        }
+        else {
+          return 'You won!';
+        }
+      }
+      else if (userChoice === 'scissors') {
+        if(computerChoice === 'rock'){
+          return 'The computer won!'
+        }
+        else {
+          return 'You won!';
+        }
+      }
+  }
+
+}
+
+const playGame = () => {
+  const userChoice = getUserChoice('scissors');
+  const computerChoice = getComputerChoice();
+  console.log('You threw: ' + userChoice);
+  console.log('The computer threw:' + computerChoice);
+  console.log( determineWinner(userChoice, computerChoice));
+};
+
+console.log(playGame());
